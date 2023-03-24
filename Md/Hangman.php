f0fb1efe5dfb9b2c0c3970e40c21165ee0944ee1
KarlisGolubovs <karlisgolubovs@gmail.com>
2023-03-24 23:19:17 +0200

    initial commit

--- Md/Hangman.php
+++ Md/Hangman.php
@@ -1,4 +1,51 @@
 <?php
 
 echo "Welcome to Hangman\n";
-echo "test";
+
+$guesses = [];
+$words = [
+    "bohr",
+    "plank",
+    "boltzmann",
+    "feynman",
+    "sagan"
+];
+
+$wordChoice = $words[array_rand($words)];
+$hiddenWord = str_repeat("_ ", strlen($wordChoice));
+echo $hiddenWord . "\n";
+
+
+$lives = 3;
+$wrongGuesses = 0;
+while ($wrongGuesses < $lives) {
+    $guess = strtolower(readline("Guess a letter: "));
+    if (!ctype_alpha($guess) || strlen($guess) != 1 || in_array($guess, $guesses)) {
+        echo "Invalid guess. Please try again.\n";
+        continue;
+    }
+
+    $guesses[] = $guess;
+
+    $correctGuess = false;
+    for ($i = 0; $i < strlen($wordChoice); $i++) {
+        if ($wordChoice[$i] == $guess) {
+            $hiddenWord[$i * 2] = $guess;
+            $correctGuess = true;
+        }
+    }
+
+    if (!$correctGuess) {
+        $wrongGuesses++;
+    }
+    echo $hiddenWord . "\n";
+    echo "Guesses: " . implode(", ", $guesses) . "\n";
+
+    if (str_replace(' ', '', str_replace('_', '', $hiddenWord)) == $wordChoice) {
+        echo "Congratulations, you guessed the word!\n";
+        break;
+    }
+}
+if ($wrongGuesses == $lives*2 - 1) {
+    echo "Sorry, you ran out of lives. The word was '$wordChoice'.\n";
+}
