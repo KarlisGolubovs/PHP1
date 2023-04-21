992b5107adad1ec65baea80001c532a675abab64
KarlisGolubovs <karlisgolubovs@gmail.com>
2023-04-21 04:04:10 +0300

    commit

---
+++ CryptoApi/app/getRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\RequestException;
+
+class getClientRequest
+{
+    protected Client $client;
+    private string $apiKey;
+
+    public function __construct(string $apiKey)
+    {
+        $this->client = new Client();
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function get(string $endpoint): string
+    {
+        try {
+            $response = $this->client->get($endpoint, [
+                'headers' => [
+                    'X-CMC_PRO_API_KEY' => 'c67a4bad-b124-44a3-8e00-42f2b015082e',
+                ],
+            ]);
+
+            if ($response->getStatusCode() !== 200) {
+                throw new \Exception("API request failed with status code: " . $response->getStatusCode());
+            }
+
+            return $response->getBody()->getContents();
+
+        } catch (RequestException $e) {
+            throw new \Exception("API request failed: " . $e->getMessage());
+
+        } catch (GuzzleException $e) {
+            throw new \Exception("Guzzle request failed: " . $e->getMessage());
+        }
+    }
+}
---
+++ CryptoApi/main.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use App\getClientRequest;
+use App\TopCryptocurrencies;
+
+// Instantiate the getClientRequest class with your API key
+$clientRequest = new getClientRequest('c67a4bad-b124-44a3-8e00-42f2b015082e');
+
+// Instantiate the TopCryptocurrencies class with the getClientRequest instance
+$topCryptos = new TopCryptocurrencies($clientRequest);
+
+// Fetch the top 10 cryptocurrencies and store the result in $cryptos
+try {
+    $cryptos = $topCryptos->getTop10();
+} catch (Exception $e) {
+}
+
+// Display the results
+echo "Top 10 Cryptocurrencies by Market Cap:\n\n";
+foreach ($cryptos as $crypto) {
+    printf("%s (%s): $%.2f\n", $crypto['name'], $crypto['symbol'], $crypto['market_cap']);
+}
